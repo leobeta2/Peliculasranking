bd19f54c5802569d9ed5e34414603f72efd295f0
leobeta2 <leobeta2@gmail.com>
2014-06-19 18:18:58 -0400

    creada la funcíon que consigue las peliculas, la de reparto y descripción

--- controller.py
+++ controller.py
@@ -1,3 +1,4 @@
+
 import sqlite3
 
 def connect():
@@ -6,9 +7,36 @@ def connect():
     return con
 
 def get_movies():
+	#consigue y ordena las peliculas
     con = connect()
     c = con.cursor()
     query = "select * from movies order by ranking asc"
     result = c.execute(query)
     movies = result.fetchall()
     return movies
+
+def get_reparto(id_movie):
+	#consigue el reparto
+	con = connect()
+    c = con.cursor()
+    query = "select stars from movies where id = ?"
+    result = c.execute(query, [id_movie])
+    reparto = result.fetchall()
+    return reparto
+
+def get_descripcion(id_movie):
+    #consigue la descripción de la respectiva película
+    con = connect()
+    c = con.cursor()
+    query = "select description from movies where id = ?"
+    result = c.execute(query, [id_movie])
+    descripcion = result.fetchall()
+    return descripcion
+
+
+
+
+
+
+
+
