aef2b7743a427d1900e2bdcc052e371545ab749f
leobeta2 <leobeta2@gmail.com>
2014-06-19 18:37:33 -0400

    se define ua función para las señales, y tambińe la función que carga las películas (entender mov y pk)

--- view_movies.py
+++ view_movies.py
@@ -1,4 +1,4 @@
-
+# _*_ coding: utf-8 -*-
 
 import sys
 from Pyside import QtGui,QtCore
@@ -19,10 +19,35 @@ class Movies(QtGui.QWidget):
 	def __init__(self, parent=None):
 		QtGui.QMainWindow()
 		self.ui.setupUi(self)
+        self.load_movies()
+        self.signals()
 		self.show()
 
+	def signals(self):
+        #todas señales
+        self.ui.btn_up.clicked.connect(self.up)
+        self.ui.btn_down.clicked.connect(self.down)
+        self.ui.table_movies.clicked.connect(self.select)
+
 	def load_movies(self):
+		#función que carga películas
 		movies = controller.get_movies()
+		rows = len(movies)
+        model = QtGui.QStandardItemModel(rows, len(self.table_columns))
+        self.ui.table_movies.setModel(model)
+        self.ui.table_movies.horizontalHeader().setResizeMode(
+        	0, self.ui.table_movies.horizontalHeader().Stretch)
+        for col, h in enumerate(self.table_columns):
+            model.setHeaderData(col, QtCore.Qt.Horizontal, h[0])
+            self.ui.table_movies.setColumnWidth(col, h[1])
+        for i, data in enumerate(movies):
+            row = [data[1], data[3], data[4], data[5], data[8]]
+            for j, field in enumerate(row):
+                index = model.index(i, j, QtCore.QModelIndex())
+                model.setData(index, field)
+            #para hacerlo más sencillo
+            model.item(i).mov = data
+            model.item(i).pk = data[0]
 
 if __name__=='__main__':
 	app = QtGui.QApplication(sys.argv)
