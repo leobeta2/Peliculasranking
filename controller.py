105649be1c901eaa9f23407098c6669d11cdc1f6
leobeta2 <leobeta2@gmail.com>
2014-06-20 00:01:20 -0400

    se modifico controoler, ventana y view movies, quedaron sólo detalles

--- controller.py
+++ controller.py
@@ -25,8 +25,17 @@ def get_reparto(id_movie):
 	reparto = result.fetchall()
 	return reparto
 
+def get_imagen(id_movie):
+	#busco mi imagen
+    con = connect()
+    c = con.cursor()
+    query = "select poster from movies where id = ?"
+    result = c.execute(query, [id_movie]) #la cargo
+    imagen = result.fetchall()
+    return imagen
+
 def get_descripcion(id_movie):
-    #consigue la descripción de la respectiva película 
+    #consigue la descripción de la respectiva película seleccionada
     con = connect()
     c = con.cursor()
     query = "select description from movies where id = ?"
@@ -34,6 +43,64 @@ def get_descripcion(id_movie):
     descripcion = result.fetchall()
     return descripcion
 
+def up(id_movie):
+    # sube la posicion de la peli e el ranking
+    exito = False
+    con = connect()
+    c = con.cursor()
+    q1 = "select ranking from movies where id = ?"
+    result1 = c.execute(q1, [id_movie])
+    r_actual = result1.fetchall()
+    r_actual = int(r_actual[0][0])
+    r_sup = r_actual - 1
+    if r_sup == 0:
+        return False 
+    q2 = "update movies set ranking = 9999 where ranking = ?"
+    q3 = "update movies set ranking =" + str(r_sup) + " where id = ?"
+    q4 = "update movies set ranking = ? where ranking = 9999"
+    try:
+        c.execute(q2, [r_sup])
+        c.execute(q3, [id_movie])
+        c.execute(q4, [r_actual])
+        con.commit()
+        exito = True
+
+    except sqlite3.Error as e:
+        exito = False
+        print "Error:", e.args[0]
+    con.close()
+    return exito
+
+
+def down(id_movie):
+    exito = False
+    con = connect()
+    c = con.cursor()
+    query0 = "select count(id) from movies"
+    result0 = c.execute(query0)
+    n = result0.fetchall()
+    q1 = "select ranking from movies where id = ?"
+    result1 = c.execute(query1, [id_movie])
+    r_actual = result1.fetchall()
+    if int(r_actual[0][0]) == int(n[0][0]):
+        return False  
+    r_actual = int(r_actual[0][0])
+    r_inf = r_actual + 1
+    q2 = "update movies set ranking = 9999 where ranking = ?"
+    q3 = "update movies set ranking =" + str(r_inf) + " where id = ?"
+    q4 = "update movies set ranking = ? where ranking = 9999"
+    try:
+        c.execute(q2, [r_inf])
+        c.execute(q3, [id_movie])
+        c.execute(q4, [r_actual])
+        con.commit()
+        exito = True
+
+    except sqlite3.Error as e:
+        exito = False
+        print "Error:", e.args[0]
+    con.close()
+    return exito
 
 
 
--- view_movies.py
+++ view_movies.py
@@ -1,54 +1,107 @@
-# _*_ coding: utf-8 -*-
-
-import sys
-from PySide import QtGui,QtCore
+#!/usr/bin/python
+# ­*­ coding: utf­8 ­*­
+import sys #importacion necesaria para acceder a la librerias de Qt
+from PySide import QtGui, QtCore
 from ventana import Ui_MainWindow
 import controller
 
-class Movies(QtGui.QWidget):
-	"""docstring for Movies"""
 
-	table_columns = (
-		(u"Titulo", 200),
-		(u"Año",100),
-		(u"Director", 150),
-		(u"Pais", 100),
-		(u"Rankink", 75),
-		)
+class Movies(QtGui.QMainWindow):
 
-	def __init__(self, parent=None):
-		QtGui.QMainWindow.__init__(self, parent)
-		self.ui = Ui_MainWindow()
-		self.load_movies()
-		self.signals()
-		self.show()
+    table_columns = (
+        (u"Título", 200),
+        (u"Año", 100),
+        (u"Director", 200),
+        (u"País", 100),
+        (u"Ranking", 75))
 
-	def signals(self):
-		#todas señales
-		self.ui.btn_up.clicked.connect(self.up)
-		self.ui.btn_down.clicked.connect(self.down)
-		self.ui.table_movies.clicked.connect(self.select)
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.load_movies()
+        self.signals()
+        self.show()
 
-	def load_movies(self):
-		#funcion que carga las peliculas
-		movies = controller.get_movies()
-		rows = len(movies)
-		model = QtGui.QStandardItemModel(rows, len(self.table_columns))
-		self.ui.table_movies.setModel(model)
-		self.ui.table_movies.horizontalHeader().setResizeMode(
-        	0, self.ui.table_movies.horizontalHeader().Stretch)
-		for col, h in enumerate(self.table_columns):
-			model.setHeaderData(col, QtCore.Qt.Horizontal, h[0])
-			self.ui.table_movies.setColumnWidth(col, h[1])
-		for i, data in enumerate(movies):
-			row = [data[1], data[3], data[4], data[5], data[8]]
-			for j, field in enumerate(row):
-				index = model.index(i, j, QtCore.QModelIndex())
-				model.setData(index, field)
-			model.item(i).mov = data
-			model.item(i).pk = data[0]
+    def signals(self):
+		#toma miss señales
+        self.ui.btn_up.clicked.connect(self.up)
+        self.ui.btn_down.clicked.connect(self.down)
+        self.ui.table_movies.clicked.connect(self.select)
 
-if __name__=='__main__':
-	app = QtGui.QApplication(sys.argv)
-	main = Movies()
-	sys.exit(app.exec_())
+    def load_movies(self):
+        #carga mi tabla movies a la ventana
+        movies = controller.get_movies()
+        rows = len(movies)
+        model = QtGui.QStandardItemModel(
+            rows, len(self.table_columns))
+        self.ui.table_movies.setModel(model)
+        self.ui.table_movies.horizontalHeader().setResizeMode(
+            0, self.ui.table_movies.horizontalHeader().Stretch)
+
+        for col, h in enumerate(self.table_columns):
+            model.setHeaderData(col, QtCore.Qt.Horizontal, h[0])
+            self.ui.table_movies.setColumnWidth(col, h[1])
+
+        for i, data in enumerate(movies):
+            row = [data[1], data[3], data[4], data[5], data[8]]
+            for j, field in enumerate(row):
+                index = model.index(i, j, QtCore.QModelIndex())
+                model.setData(index, field)
+            #Parametros ocultos
+            model.item(i).mov = data
+            model.item(i).pk = data[0]
+
+    def up(self):
+		#para subir la pelicula
+        model = self.ui.table_movies.model()
+        index = self.ui.table_movies.currentIndex()
+        if index.row() == -1:  
+            self.errorMessageDialog = QtGui.QErrorMessage(self)
+            self.errorMessageDialog.showMessage("Seleccione fila!!!")
+            return False
+        else:
+            id_movie = model.item(index.row()).pk
+            if (controller.up(id_movie)):
+                self.load_movies()
+                return True
+            else:
+                return False
+
+    def down(self):
+		# lo baja en el ranking
+        model = self.ui.table_movies.model()
+        index = self.ui.table_movies.currentIndex()
+        if index.row() == -1:  
+            self.errorMessageDialog = QtGui.QErrorMessage(self)
+            self.errorMessageDialog.showMessage("Seleccione fila!!!")
+            return False
+        else:
+            id_movie = model.item(index.row()).pk
+            if (controller.down(id_movie)):
+                self.load_movies()
+                return True
+            else:
+                return False
+
+    def select(self):
+		#recargo la informacion, para despues cargarla en la ventana
+        model = self.ui.table_movies.model()
+        index = self.ui.table_movies.currentIndex()
+        id_movie = model.item(index.row()).pk
+        rep = controller.get_reparto(id_movie)
+        rep = str(rep[0][0])
+        self.ui.lbl_stars.setText(rep)
+        des = controller.get_descripcion(id_movie)
+        des = str(des[0][0])
+        self.ui.lbl_description.setText(des)
+        im = controller.get_imagen(id_movie)#busco mi imagen
+        im = str(im[0][0])
+        self.ui.lbl_imagen.setPixmap(im)# cargo mi imagen
+
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    main = Movies()
+    sys.exit(app.exec_())
